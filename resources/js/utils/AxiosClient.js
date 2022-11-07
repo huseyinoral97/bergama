2b6866eea45c011dcb4a539820150af050aec75c
huseyinoral97 <82272445+huseyinoral97@users.noreply.github.com>
2022-11-07 16:31:36 +0300

    axios

--- resources/js/utils/AxiosClient.js
+++ resources/js/utils/AxiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const AxiosClient = axios.create({
-    baseURL: `http://localhost:8000/`,
+    baseURL: `http://ec2-18-209-60-137.compute-1.amazonaws.com/`,
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
